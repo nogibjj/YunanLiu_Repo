e8b5eda34380dddedbb1484b8932fcc538e4d8a1
YunanLiu06 <112599210+YunanLiu06@users.noreply.github.com>
2022-09-27 02:01:57 +0000

    mod dockerfile, querydb.py

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,12 @@
-FROM alpine:latest
-RUN apk update && apk add bash
+FROM python:3.7.3-stretch
 
+# Working Directory
 WORKDIR /app
-COPY repeat.sh /app
+
+# Copy source code to working directory
+COPY . docker_app_tryout.py /app/
+
+# Install packages from requirements.txt
+# hadolint ignore=DL3013
+RUN pip install --no-cache-dir --upgrade pip &&\
+    pip install --no-cache-dir --trusted-host pypi.python.org -r requirements.txt
--- querydb.py
+++ querydb.py
@@ -16,4 +16,21 @@ def querydb(query="SELECT * FROM default.co2_emission_by_countries_csv LIMIT 2")
             print(row)
 
     return result
-querydb()
+
+def sortbyyearDESC(query="SELECT * FROM default.co2_emission_by_countries_csv ORDER BY _c3 DESC LIMIT 5"):
+    with sql.connect(
+        server_hostname=os.getenv("DATABRICKS_SERVER_HOSTNAME"),
+        http_path=os.getenv("DATABRICKS_HTTP_PATH"),
+        access_token=os.getenv("DATABRICKS_TOKEN"),
+    ) as connection:
+
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            result = cursor.fetchall()
+
+        for row in result:
+            print(row)
+
+    return result
+
+sortbyyear()
